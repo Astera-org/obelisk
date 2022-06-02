d802f429d9dd22402e49febcf7c529c58e092345
mich <mich@astera.org>
2022-06-02 15:02:18 -0700

    handled edge case where agent disconnects, and switches to heuristic

--- worlds/integrated/fworld/fworld.go
+++ worlds/integrated/fworld/fworld.go
@@ -1513,7 +1513,9 @@ func (ev *FWorld) ConfigWorldGui() *gi.Window {
 				// Step the agent
 				actions, _ := agent.Step(defaultCtx, observations, "episode:"+strconv.Itoa(ev.Tick.Cur))
 				move, ok := actions["move"] // This contains a discrete option.
-
+				if actions == nil {
+					fmt.Println("disconnected from agent, exiting")
+				}
 				if ok {
 					// We've received a discrete action and can use it directly to StepWorld.
 					ev.StepWorld(int(move.DiscreteOption), false)
