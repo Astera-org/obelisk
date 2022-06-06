fd228d213c5ca708f951f1c909c81e80a5764e92
jed <jed@astera.org>
2022-06-06 10:32:12 -0700

    .

--- infra/database.sql
+++ infra/database.sql
@@ -6,7 +6,7 @@ use obelisk;
 drop table jobs;
 create table jobs (job_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, user_id INT DEFAULT 0, 
     search_id INT DEFAULT 0, status INT DEFAULT 0, priority INT DEFAULT 0,
-    time_added TIMESTAMP DEFAULT NOW(), worker_name VARCHAR(10), instance_name VARCHAR(10),
+    time_added TIMESTAMP DEFAULT NOW(), worker_name VARCHAR(10),
     time_start TIMESTAMP DEFAULT 0, time_end TIMESTAMP DEFAULT 0,
     model_name VARCHAR(10),model_version INT DEFAULT 0,
     env_name VARCHAR(10),env_version INT DEFAULT 0,
@@ -14,4 +14,6 @@ create table jobs (job_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, user_id INT D
     model_param LONGBLOB, env_param LONGBLOB, bail BOOLEAN, score DOUBLE DEFAULT 0);
 
 create table users (user_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, user_name VARCHAR(10));
+
+
 
