df40393c68faeaf4c7b22372cbfd96206339cc7c
Erdal <etuleu@gmail.com>
2022-07-05 13:42:47 -0700

    make the sql script idempotent and update readme with setup instructions

--- infra/database.sql
+++ infra/database.sql
@@ -1,10 +1,11 @@
+drop database if exists obelisk;
 
 create database obelisk;
 use obelisk;
 
 #status (0-pending,1-working,2-complete,3-errored)
 #priority lower priority numbers take precedence
-drop table jobs;
+drop table if exists jobs;
 create table jobs (job_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, user_id INT DEFAULT 0, 
     search_id INT DEFAULT 0, status INT DEFAULT 0, priority INT DEFAULT 0,
     callback VARCHAR(30), time_added TIMESTAMP DEFAULT NOW(), 
@@ -20,11 +21,14 @@ create table jobs (job_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, user_id INT D
 
 #type (0-agent,1-world)
 #status (0-current,1-archived,2-validating,3-regressed)
+drop table if exists binaries;
 create table binaries (bin_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(10), version VARCHAR(10), 
     package_hash VARCHAR(40), time_added TIMESTAMP DEFAULT NOW(), type INT DEFAULt 0, status INT DEFAULT 0);
 
 create table notes (note_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,job_id INT,user_id INT,note_text VARCHAR(1024) );
 
+drop table if exists cfgs;
+drop table if exists users;
 create table users (user_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(10));
 
 
