9eca61a42559651c8c761fb3184e95b96a0453cd
Erdal T <etuleu@gmail.com>
2022-07-11 16:37:11 -0700

    Fix build infra (#115)
    * rename workflow file to build
    
    * add package name

--- infra/generate_thrift.sh
+++ infra/generate_thrift.sh
@@ -1,5 +1,5 @@
 mkdir -p gengo
 mkdir -p genjs
 
-thrift -r --gen go -out gengo/ infra.thrift
+thrift -r --gen go:package_prefix=github.com/Astera-org/obelisk/infra/gengo/ -out gengo/ infra.thrift
 thrift -r --gen js -out genjs/ infra.thrift
