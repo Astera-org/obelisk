5acdb3c6664c61f222d41138c465d55f5469f1fb
Erdal <etuleu@gmail.com>
2022-07-12 14:21:56 -0700

    upload binary webpage

---
+++ infra/jobboard/upload.js
@@ -0,0 +1,36 @@
+
+const FILE_UPLOAD_URL = "http://localhost:8080/addBinary";
+
+async function addBinary(file) {
+    console.log("addBinary ", file);
+
+    const formData = new FormData();
+    formData.append("binary", file);
+
+    const response = await fetch(FILE_UPLOAD_URL, {
+        method: "POST",
+        body: formData
+    });
+
+    console.log('file uploaded response', response);
+}
+
+$(function() {
+    console.log("document ready");
+
+    // setup add job form
+    $("#upload_form").submit(function(event) {
+        event.preventDefault();
+
+        const form = event.target;
+        const input = form[0];
+        const files = input.files;
+
+        if (files.length) {
+            addBinary(files[0]);
+        } else {
+            console.error("Please select a file first");
+        }
+    });
+
+});
