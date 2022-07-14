c7ec9cce01960787a41cffd2d4a850d09f4a4d37
Andrew Keenan Richardson <richardson.andrew@gmail.com>
2022-07-14 13:05:06 -0700

    Move optimizer thrift server into infra folder

--- models/optimize/generate_thrift.sh
+++
@@ -1,8 +0,0 @@
-# we don't use the default gen-py name because python hates hyphens on packages
-mkdir -p genpy
-mkdir -p gengo
-#mkdir -p gencpp
-
-thrift -r --gen py -out genpy/ optimize.thrift
-thrift -r --gen go:package_prefix=github.com/Astera-org/obelisk/models/optimize/gengo/ -out gengo/ optimize.thrift
-#thrift -r --gen cpp -out gencpp/ optimize.thrift
