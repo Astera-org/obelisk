a5e79e35b3f2990fb8198c1091100b01ff57beb4
Erdal <etuleu@gmail.com>
2022-07-15 15:18:33 -0700

    Fix package prefix in generate_thrift

--- infra/optimizer/generate_thrift.sh
+++ infra/optimizer/generate_thrift.sh
@@ -4,5 +4,5 @@ mkdir -p gengo
 #mkdir -p gencpp
 
 thrift -r --gen py -out genpy/ optimize.thrift
-thrift -r --gen go:package_prefix=github.com/Astera-org/obelisk/models/optimize/gengo/ -out gengo/ optimize.thrift
+thrift -r --gen go:package_prefix=github.com/Astera-org/obelisk/infra/optimizer/gengo/ -out gengo/ optimize.thrift
 #thrift -r --gen cpp -out gencpp/ optimize.thrift
