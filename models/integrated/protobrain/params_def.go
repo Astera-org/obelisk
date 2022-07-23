417d482890ac5a86587414cafbd9020689e7ac42
mich <mich@astera.org>
2022-07-23 12:05:48 -0700

    add ALL the optional hyperparameters specified by randy

--- models/integrated/protobrain/params_def.go
+++ models/integrated/protobrain/params_def.go
@@ -14,6 +14,25 @@ func createParams(gconfig Config) params.Sets {
 	lrate := fmt.Sprintf("%f", gconfig.LRATE)
 	act_init := fmt.Sprintf("%f", gconfig.ACTAVG_INIT)
 
+	layerDecayAct := fmt.Sprintf("%f", gconfig.LayerDecayAct)
+	layerDecayGlong := fmt.Sprintf("%f", gconfig.LayerDecayGlong)
+	layerClampGe := fmt.Sprintf("%f", gconfig.LayerClampGe)
+	//Specific to Proto Layers
+	layerTRCDriveScale := fmt.Sprintf("%f", gconfig.LayerTRCDriveScale)
+	smaLayerActNoiseGe := fmt.Sprintf("%f", gconfig.SMALayerActNoiseGe)
+	smaLayerActNoiseGi := fmt.Sprintf("%f", gconfig.SMALayerActNoiseGi)
+	//Projections
+	prjnSWtAdaptLrate := fmt.Sprintf("%f", gconfig.PrjnSWtAdaptLrate)
+	prjnSWtAdaptDreamVar := fmt.Sprintf("%f", gconfig.PrjnSWtAdaptDreamVar)
+	prjnLearnXCalPThrMin := fmt.Sprintf("%f", gconfig.PrjnLearnXCalPThrMin)
+	prjnLearnXCalLrnThr := fmt.Sprintf("%f", gconfig.PrjnLearnXCalLrnThr)
+	backPrjnScaleRel := fmt.Sprintf("%f", gconfig.BackPrjnScaleRel)
+	ctBackPrjnScaleRel := fmt.Sprintf("%f", gconfig.CTBackPrjnScaleRel)
+	inhibPrjnLearnLrateBase := fmt.Sprintf("%f", gconfig.InhibPrjnLearnLrateBase)
+	inhibPrjnScaleAbs := fmt.Sprintf("%f", gconfig.InhibPrjnScaleAbs)
+	lateralPrjnScaleRel := fmt.Sprintf("%f", gconfig.LateralPrjnScaleRel)
+	fmPulPrjnScaleRel := fmt.Sprintf("%f", gconfig.FmPulPrjnScaleRel)
+
 	// ParamSets is the default set of parameters -- Base is always applied, and others can be optionally
 	// selected to apply on top of that
 	var ParamSets = params.Sets{
@@ -28,16 +47,15 @@ func createParams(gconfig Config) params.Sets {
 						"Layer.Inhib.Layer.FFEx0": "0.15",
 						"Layer.Inhib.Layer.FFEx":  "0.02", //
 						//"Layer.Act.Gbar.L":        "0.2", // NOT IMPORTANT
-						"Layer.Act.Decay.Act":   "0.2", // 8 [0 - 1]
-						"Layer.Act.Decay.Glong": "0.6", // 8 [0 - 1]
-						"Layer.Act.Clamp.Ge":    "1.0", // 4 Probs fine  [.6 1.5]
+						"Layer.Act.Decay.Act":   layerDecayAct,   // 8 [0 - 1]
+						"Layer.Act.Decay.Glong": layerDecayGlong, // 8 [0 - 1]
+						"Layer.Act.Clamp.Ge":    layerClampGe,    // 4 Probs fine  [.6 1.5]
 						"Layer.Inhib.Pool.Gi":   "1.1",
 					}},
 				{Sel: ".Hidden", Desc: "noise? sub-pools",
 					Params: params.Params{
 						"Layer.Inhib.ActAvg.Init":    "0.06",
 						"Layer.Inhib.ActAvg.AdaptGi": "false", // no!
-						"Layer.Inhib.Pool.Gi":        "1.1",
 						"Layer.Inhib.Pool.On":        "true",
 						"Layer.Inhib.Layer.On":       "true", // full layer
 					}},
@@ -55,9 +73,9 @@ func createParams(gconfig Config) params.Sets {
 					}},
 				{Sel: "TRCLayer", Desc: "",
 					Params: params.Params{ //LEAVE TRCLAYER alone except for trc.drivescale
-						"Layer.TRC.DriveScale":   "0.15", // 10  .3 - .05
-						"Layer.Act.Decay.Act":    "0.5",  //LEAVE THIS ON
-						"Layer.Act.Decay.Glong":  "1",    // clear long
+						"Layer.TRC.DriveScale":   layerTRCDriveScale, // 10  .3 - .05
+						"Layer.Act.Decay.Act":    "0.5",              //LEAVE THIS ON
+						"Layer.Act.Decay.Glong":  "1",                // clear long
 						"Layer.Inhib.Pool.FFEx":  "0.0",
 						"Layer.Inhib.Layer.FFEx": "0.0",
 					}},
@@ -149,8 +167,8 @@ func createParams(gconfig Config) params.Sets {
 				{Sel: "#SMA", Desc: "",
 					Params: params.Params{
 						"Layer.Act.Noise.On": "true",
-						"Layer.Act.Noise.Ge": "0.001", //10 .0005 - .01
-						"Layer.Act.Noise.Gi": "0.001", //10 .0005 - .01
+						"Layer.Act.Noise.Ge": smaLayerActNoiseGe, //10 .0005 - .01
+						"Layer.Act.Noise.Gi": smaLayerActNoiseGi, //10 .0005 - .01
 					}},
 				{Sel: "#SMAP", Desc: "pulv",
 					Params: params.Params{
@@ -187,26 +205,26 @@ func createParams(gconfig Config) params.Sets {
 
 				{Sel: "Prjn", Desc: "norm and momentum on is critical, wt bal not as much but fine",
 					Params: params.Params{
-						"Prjn.Learn.Lrate.Base":       lrate,   // 10  [.001, .1, .5]  .01 for SynSpkTheta
-						"Prjn.SWt.Adapt.Lrate":        "0.001", // 8 [.0001 - .01]  0.001 seems to work fine, but .001 maybe more reliable
-						"Prjn.SWt.Adapt.DreamVar":     "0.01",  // 8 [0 - .05]  0.01 is just tolerable
-						"Prjn.SWt.Init.SPct":          "1.0",   // .5 ok here, 1 best for larger nets: objrec, lvis
-						"Prjn.Learn.KinaseCa.SpikeG":  "12",    // 12 matches theta exactly, higher dwtavg but ok
+						"Prjn.Learn.Lrate.Base":       lrate,                // 10  [.001, .1, .5]  .01 for SynSpkTheta
+						"Prjn.SWt.Adapt.Lrate":        prjnSWtAdaptLrate,    // 8 [.0001 - .01]  0.001 seems to work fine, but .001 maybe more reliable
+						"Prjn.SWt.Adapt.DreamVar":     prjnSWtAdaptDreamVar, // 8 [0 - .05]  0.01 is just tolerable
+						"Prjn.SWt.Init.SPct":          "1.0",                // .5 ok here, 1 best for larger nets: objrec, lvis
+						"Prjn.Learn.KinaseCa.SpikeG":  "12",                 // 12 matches theta exactly, higher dwtavg but ok
 						"Prjn.Learn.KinaseCa.NMDAG":   "1",
 						"Prjn.Learn.KinaseCa.Rule":    "SynSpkTheta", //REMOVE
 						"Prjn.Learn.KinaseCa.MTau":    "5",           // 5 > 10 test more
 						"Prjn.Learn.KinaseCa.UpdtThr": "0.05",        // 0.05 -- was LrnThr
 						"Prjn.Learn.XCal.On":          "true",
-						"Prjn.Learn.XCal.PThrMin":     "0.05", // 8 [0.01 - .1]  .05 > .01 for PCA for SynSpk, bad for NeurSpk
-						"Prjn.Learn.XCal.LrnThr":      "0.05", // 8 [0.01 - .1] .05 > .01 here but not smaller nets -- should match NeurCa.LrnThr 0.05 also good
+						"Prjn.Learn.XCal.PThrMin":     prjnLearnXCalPThrMin, // 8 [0.01 - .1]  .05 > .01 for PCA for SynSpk, bad for NeurSpk
+						"Prjn.Learn.XCal.LrnThr":      prjnLearnXCalLrnThr,  // 8 [0.01 - .1] .05 > .01 here but not smaller nets -- should match NeurCa.LrnThr 0.05 also good
 					}},
 				{Sel: ".Back", Desc: "top-down back-projections MUST have lower relative weight scale, otherwise network hallucinates",
 					Params: params.Params{
-						"Prjn.PrjnScale.Rel": "0.1", //10 [.05 - .3]
+						"Prjn.PrjnScale.Rel": backPrjnScaleRel, //10 [.05 - .3]
 					}},
 				{Sel: ".CTBack", Desc: "deep top-down -- stronger",
 					Params: params.Params{
-						"Prjn.PrjnScale.Rel": "0.2", //10 [.05 - .3] 0.2 > 0.5 - .ctback ad acttoct should have shared hyperparam
+						"Prjn.PrjnScale.Rel": ctBackPrjnScaleRel, //10 [.05 - .3] 0.2 > 0.5 - .ctback ad acttoct should have shared hyperparam
 					}},
 				{Sel: ".ActToCT", Desc: "weaker",
 					Params: params.Params{
@@ -214,20 +232,20 @@ func createParams(gconfig Config) params.Sets {
 					}},
 				{Sel: ".Inhib", Desc: "inhibitory projection",
 					Params: params.Params{
-						"Prjn.Learn.Learn":      "true",  // learned decorrel is good
-						"Prjn.Learn.Lrate.Base": "0.001", // 9 [.0001 - .01] .0001 > .001 -- slower better!
+						"Prjn.Learn.Learn":      "true",                  // learned decorrel is good
+						"Prjn.Learn.Lrate.Base": inhibPrjnLearnLrateBase, // 9 [.0001 - .01] .0001 > .001 -- slower better!
 						"Prjn.SWt.Init.Var":     "0.0",
 						"Prjn.SWt.Init.Mean":    "0.1",
 						"Prjn.SWt.Init.Sym":     "false",
 						"Prjn.SWt.Adapt.On":     "false",
-						"Prjn.PrjnScale.Abs":    "0.3", // 9 [0 - 0.5] .1 = .2, slower blowup
-						"Prjn.IncGain":          "1",   //TODO randy take a look, ill poke randy  .5 def
+						"Prjn.PrjnScale.Abs":    inhibPrjnScaleAbs, // 9 [0 - 0.5] .1 = .2, slower blowup
+						"Prjn.IncGain":          "1",               //TODO randy take a look, ill poke randy  .5 def
 					}},
 				{Sel: ".Lateral", Desc: "default for lateral -- not using", //DO NOT REMOVE
 					Params: params.Params{
 						"Prjn.SWt.Init.Sym":  "false",
 						"Prjn.SWt.Init.Var":  "0",
-						"Prjn.PrjnScale.Rel": "0.02", // .02 > .05 == .01 > .1  -- very minor diffs on TE cat
+						"Prjn.PrjnScale.Rel": lateralPrjnScaleRel, // .02 > .05 == .01 > .1  -- very minor diffs on TE cat
 					}},
 				{Sel: ".CTFmSuper", Desc: "CT from main super", //DO NOT REMOVE
 					Params: params.Params{
@@ -240,7 +258,7 @@ func createParams(gconfig Config) params.Sets {
 					}},
 				{Sel: ".FmPulv", Desc: "default for pulvinar",
 					Params: params.Params{
-						"Prjn.PrjnScale.Rel": "0.1", //10 [.05 - .3] .1 > .2
+						"Prjn.PrjnScale.Rel": fmPulPrjnScaleRel, //10 [.05 - .3] .1 > .2
 					}},
 				{Sel: ".CTSelf", Desc: "CT to CT",
 					Params: params.Params{ //DO NOT REMOVE
